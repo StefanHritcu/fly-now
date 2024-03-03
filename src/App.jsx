25692d1061f2805858916fa4a34ad6db4362de92
StefFork <cepostaperstefan@gmail.com>
2024-03-03 12:10:38 +0100

    added: initial files project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1>Home Page</h1>
+        <Link to="/shop">Vai allo Shop</Link>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Shop.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Shop() {
+    return(
+        <>
+        <div>
+            <h1>Welcome in the shop section</h1>
+            <Link to="/">indietro</Link>
+        </div>
+        </>
+    )
+}
+export default Shop
